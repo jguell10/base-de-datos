44e5679778e082f8f5701c1dd85be02ea347c205
jguell10 <jguell10@hotmail.com>
2024-08-26 09:13:50 -0500

    rotacion de producto mas vendido

---
+++ Rotacion_Productos.sql
@@ -0,0 +1,8 @@
+﻿select top 1 p.nombre producto,ped.entregaciudad ciudad, sum(pd.cantidad) cantidad
+from pedidosdetalle pd
+inner join productos p ON pd.idproducto = p.idproducto
+inner join pedidos ped ON pd.idpedido = ped.idpedido
+inner join proveedores prov ON p.idproveedor = prov.idproveedor
+where prov.ciudad = 'Melbourne'
+group by p.nombre, ped.entregaciudad
+order by 3 desc
